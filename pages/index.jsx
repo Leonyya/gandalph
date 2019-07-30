80fa8ce3e564de1fd27854d90e18e9695519bff2
Luis Leon <ing.lulb@icloud.com>
2019-07-29 21:06:24 -0500

    Secret anonymous auth

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,89 +4,55 @@ import fetch from 'isomorphic-fetch'
 import Link from 'next/link'
 import Head from 'next/head'
 import Nav from '../components/nav'
+import Connect from '../components/Connect'
+import firebase from 'firebase'
+
+let firebaseConfig = {
+	apiKey: "AIzaSyBCyovJbc8AuTHaCOJI070fo-Q7YG6VZrQ",
+	authDomain: "infinity-2020.firebaseapp.com",
+	databaseURL: "https://infinity-2020.firebaseio.com",
+	projectId: "infinity-2020",
+	storageBucket: "",
+	messagingSenderId: "1064260992291",
+	appId: "1:1064260992291:web:8181fee94896c0a7"
+};
+try {
+	firebase.initializeApp(firebaseConfig)
+} catch(err) {
+	if(!/already exists/.test(err.message)) {
+		console.log('Firebase initialization error',err.stack)
+	}
+}
+
+const database = firebase.database()
 
 class Home extends Component {
-	// acá pedimos los datos de los mensajes viejos, esto se ejecuta tanto en el cliente como en el servidor
-	static async getInitialProps ({ req }) {
-		const response = await fetch('http://localhost:3000/messages')
-		const messages = await response.json()
-		return { messages }
-	}
-
-	static defaultProps = {
-		messages: []
-	}
-
-	// en el estado guardamos un string vacío (el campo del formulario) y los mensajes que recibimos del API
-	state = {
-		field: '',
-		messages: this.props.messages
-	}
-
-	// una vez que el componente se montó en el navegador nos conectamos al servidor de sockets
-	// y empezamos a recibimos el evento `message` del servidor
-	componentDidMount () {
-		this.socket = io('http://localhost:3000/')
-		this.socket.on('message', this.handleMessage)
-	}
-
-	// cuando el componente se va a desmontar es importante que dejemos de escuchar el evento
-	// y que cerremos la conexión por sockets, esto es para evitar problemas de que lleguen mensajes
-	componentWillUnmount () {
-		this.socket.off('message', this.handleMessage)
-		this.socket.close()
-	}
-
-	// cuando llega un mensaje del servidor lo agregamos al estado de nuestra página
-	handleMessage = (message) => {
-		this.setState(state => ({ messages: state.messages.concat(message) }))
-	}
-
-	// cuando el valor del input cambia actualizamos el estado de nuestra página
-	handleChange = event => {
-		this.setState({ field: event.target.value })
-	}
-
-	// cuando se envía el formulario enviamos el mensaje al servidor
-	handleSubmit = event => {
-		event.preventDefault()
-
-		// creamos un objeto message con la fecha actual como ID y el valor del input
-		const message = {
-			id: (new Date()).getTime(),
-			value: this.state.field
+	constructor(props) {
+		super(props)
+		this.state = {
+			name: 'Leon'
 		}
-
-		// enviamos el objeto por socket al servidor
-		this.socket.emit('message', message)
-
-		// lo agregamos a nuestro estado para que se muestre en pantalla y limpiamos el input
-		this.setState(state => ({
-			field: '',
-			messages: state.messages.concat(message)
-		}))
 	}
+
+	componentDidMount () {
+		const nameRef= database.ref().child('name')
+		nameRef.on('value', snapshot => {
+			this.setState({
+				name: snapshot.val()
+			})
+		})
+	}
+
+	componentWillUnmount () {
+	}
+
 
 	render () {
 		return (
-			<main>
-				<div>
-					<ul>
-						{this.state.messages.map(message =>
-							<li key={message.id}>{message.value}</li>
-						)}
-					</ul>
-					<form onSubmit={this.handleSubmit}>
-						<input
-							onChange={this.handleChange}
-							type='text'
-							placeholder='Hola Platzi!'
-							value={this.state.field}
-						/>
-						<button>Enviar</button>
-					</form>
-				</div>
-			</main>
+			<div>
+				<Connect/>
+				<h1>{this.state.name}</h1>
+			</div>
 		)
 	}
 }
