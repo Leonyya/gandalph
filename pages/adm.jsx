5c8f6b9bf87ebe9243c13ef3c4ff76beef308768
Luis Leon <ing.lulb@icloud.com>
2019-11-09 21:42:23 -0600

    cpanel style modified

--- pages/adm.jsx
+++ pages/adm.jsx
@@ -3,6 +3,7 @@ import * as firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
 
+
 class AdminPanel extends Component {
   constructor(props) {
     super(props)
@@ -10,15 +11,14 @@ class AdminPanel extends Component {
   render() {
     return (
       <div className="container">
-            <div className="cpanelForm card text-center w-50">
+            <div className="cpanelForm card text-center w-50 mb-2">
               <div className="card-body">
                 <h5 className="card-title">Hangoutmybrowser control panel</h5>
-                <p className="card-text">v0.1 branch feature/globalhandler</p>
                 <form>
                   <div className="form-group">
                     <label for="exampleInputEmail1">Email address</label>
                     <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                    <small id="emailHelp" className="form-text text-muted">Only privilege people got access</small>
                   </div>
                   <div className="form-group">
                     <label for="exampleInputPassword1">Password</label>
@@ -28,14 +28,20 @@ class AdminPanel extends Component {
                     <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
                     <label className="form-check-label" for="exampleCheck1">Remember me</label>
                   </div>
-                  <button type="submit" className="btn btn-primary">Submit</button>
+                  <button type="submit" className="btn btn-primary">Log in</button>
                 </form>
               </div>
-          </div>
-          <style jsx>{`
-            .cpanelForm {
-              background-image: linear-gradient(to top, #ffffff, #eeedfa, #dadcf5, #c5cbf1, #acbced, #9bb1ec, #89a6ea, #759ce9, #6993ea, #5e89eb, #5480eb, #4a76eb);              color:white;
 
+          </div>
+          <style jsx global>{`
+              body {
+                background-image:url("/static/RollingWaves.jpg");
+              }
+            .cpanelForm {
+              margin-top: 5%;
+              background: linear-gradient(0deg, rgba(0,0,0,0.5537757437070938) 0%, rgba(0,0,0,1) 100%);
+              color:white;
+              left:30%;
             }
           `}</style>
       </div>
