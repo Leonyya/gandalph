20bd41d1ec86ba60f0c591a48a9163bd98b8af54
Luis Leon <ing.lulb@icloud.com>
2019-11-11 01:42:46 -0600

    React Store container created

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,54 @@
 import React from 'react'
 import App, { Container } from 'next/app'
 import Head from 'next/head'
+import { createStore, combineReducers, compose } from 'redux'
+import { Provider } from 'react-redux'
+import withRedux from 'next-redux-wrapper'
+import { db, auth } from '../firebase/config'
+
+
+const reducer = (state = {foo:''}, action) => {
+  switch(action.type){
+    case 'FOO':
+      return { ...state, foo: action.payload}
+    default:
+      return state
+  }
+}
+
+const makeStore = (initialState, options) => {
+  return createStore(reducer, initialState)
+}
 
 class MyApp extends App {
+  static async getInitialProps({Component, ctx}) {
+    ctx.store.dispatch({type: 'FOO', payload: 'foo'})
+    const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {}
+    return {pageProps}
+  }
   componentDidMount() {
   }
 
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps, store } = this.props;
 
     return (
-      <Container>
-        <Head>
-          <title>Hang out there</title>
-          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
-          <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        </Head>
-        <Component {...pageProps} />
-      </Container>
+
+        <Container>
+          <Head>
+            <title>Hang out there</title>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+          </Head>
+          <Provider store={store}>
+            <Component {...pageProps} />
+          </Provider>
+        </Container>
+
     );
   }
 }
 
-export default MyApp
+export default withRedux(makeStore)(MyApp)
