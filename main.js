25e91fc921dce0c2bbf90a645b75e660bf082ad4
Luis Leon <ing.lulb@icloud.com>
2019-12-03 15:02:03 -0600

    Dev server and mqtt startup in file main setup

--- main.js
+++ main.js
@@ -2,52 +2,84 @@ const mosca = require('mosca')
 const blessed = require('blessed')
 const express = require('express')
 const { exec } = require('child_process');
+const next = require('next')
 
-exec('npm run build', (err, stdout, stderr) => {
-  if (err) {
-    return;
-  }
 
-  // the *entire* stdout and stderr (buffered)
-  console.log(`stdout: ${stdout}`);
-  console.log(`stderr: ${stderr}`);
-});
 
-/*let screen = blessed.screen({
-    smartCSR: true
-})
-screen.title = 'mqBot deploying'
+if( process.argv[2] && process.argv[2] == "dev") {  
 
-let box = blessed.box({
-    top: 'center',
-    left: 'center',
-    width: '50%',
-    height: '50%',
-    content: 'mqBot setting up',
-    tags: true,
-    border: {
-        type:'line'
-    },
-    style: {
-        fg: 'white',
-        bg: 'magenta',
-        border: {
-            fg: '#f0f0f0'
-        },
-        hover: {
-            bg:'green'
+    const dev = process.env.NODE_ENV !== 'production'
+    const app = next({ dev })
+    const handle = app.getRequestHandler()
+
+    app.prepare()
+    .then(() => {
+      const server = express()
+        
+      server.get('*', (req, res) => {
+        return handle(req, res)
+      })
+        
+      server.listen(3000, (err) => {
+        if (err) throw err
+        console.log('> Dev server ready on http://localhost:3000')
+      })
+    })
+    .catch((ex) => {
+      console.error(ex.stack)
+      process.exit(1)
+    })
+} else {
+    exec('npm run build', (err, stdout, stderr) => {
+        if (err) {
+            return
         }
-    }
+    
+        // the *entire* stdout and stderr (buffered)
+        console.log(`${stdout}`)
+        console.log(`stderr: ${stderr}`)
+        console.log('> Client built')
+        console.log('> In a few minuts will become available at http://localhost:3000/')
+    
+        exec('npm run start', (err,stdout,stderr) => {
+            if(err) {
+                return
+            }
+            console.log(`${stdout}`);
+            console.log('> Wapp deployed')
+            console.log(`stderr: ${stderr}`)
+      })
+    });
+}
+
+let ascoltatore = {
+    type: 'redis',
+    redis: require('redis'),
+    db: 12,
+    port: 6379,
+    return_buffers: true,
+    host: "localhost"
+}
+
+let moscaSettings = {
+    port: 1883,
+    backend: ascoltatore,
+}
+
+let server = new mosca.Server(moscaSettings)
+server.on('ready', ()=> {
+    console.log('> MQTT broker is ready at ::1883')
 })
 
-// Entry render settings 
-screen.append(box)
-box.focus()
-screen.render()
+
+server.on('clientConnected', function(client) {
+    console.log('client connected', client.id)
+})
+
+server.on('published', function(packet, client){
+    console.log('Published', packet.topic, packet.payload)
+})
 
 
-// Escape rutine
-screen.key(['escape', 'q', 'C-c'], function(ch, key) {
-    return process.exit(0);
-  });
-*/
+
+
